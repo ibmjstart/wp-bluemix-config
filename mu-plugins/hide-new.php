c368af523be25bfbafd7cae699e545be124ae087
Miguel Clement <miguelfclement@gmail.com>
2015-02-12 17:04:58 -0500

    Update hide-new.php

--- mu-plugins/hide-new.php
+++ mu-plugins/hide-new.php
@@ -4,5 +4,7 @@ function hide_add($hook){
   if($hook == 'theme-install.php' || $hook =='plugin-install.php'){
     wp_enqueue_script( 'hide-theme', plugins_url('hide.js', __FILE__), 'jquery');
   }
+  else
+    wp_enqueue_script( 'add-logo', plugins_url('logo.js', __FILE__), 'jquery');
 }
 add_action('admin_enqueue_scripts', 'hide_add');
